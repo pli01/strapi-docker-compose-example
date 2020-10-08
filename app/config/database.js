5c61ffbcb0c07a71d3bfb8d9388e7a3d9315d770
pli01 <pli01@noreply.github.com>
2020-10-08 22:33:50 +0200

    DATABASE_URI

--- app/config/database.js
+++ app/config/database.js
@@ -2,15 +2,11 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'strapi-hook-mongoose',
+      connector: 'mongoose',
       settings: {
         client: 'mongo',
         uri: env('DATABASE_URI', ''),
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi-production'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD' , ''),
+        database: env('DATABASE_NAME', 'strapi'),
       },
       options: {
         ssl: env('DATABASE_SSL', false),
